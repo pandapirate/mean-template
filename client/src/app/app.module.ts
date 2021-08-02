8bb9a188f2549b85637e6b89651cb16603712bcf
Kevin Huang <kevin@ark7.com>
2021-08-02 11:01:16 -0700

    Add angular material

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import {MatButtonModule} from '@angular/material/button';
 import { NgModule } from '@angular/core';
 
 @NgModule({
@@ -11,7 +13,9 @@ import { NgModule } from '@angular/core';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    MatButtonModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
